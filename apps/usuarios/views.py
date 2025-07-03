3089aba10648b5e3513c94717bdfe54484c0ef9c
Edilson Oliveira <edilsonoliveira@me.com>
2025-07-03 08:25:54 -0300

    Created login

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib import auth
 
 
 def cadastro(request):
@@ -33,3 +35,21 @@ def cadastro(request):
     )
 
     return redirect('login')
+
+
+def login(request):
+  if request.method == 'GET':
+    return render(request, 'login.html')
+  elif request.method == 'POST':
+    username = request.POST.get('username')
+    senha = request.POST.get('senha')
+
+    user = authenticate(request, username=username, password=senha)
+
+    if user:
+      auth.login(request, user)
+      return redirect('/mentorados/')
+    
+    messages.add_message(request, constants.ERROR, 'Usuario ou senha invalidos!')
+    return redirect('login')
+
